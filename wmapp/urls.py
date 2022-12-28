7411355a250495d94ecca945f017a68390510e2e
Irene Bravo <irene.bravo.user@gmail.com>
2022-12-28 22:45:00 +0100

    liked songs page added

--- wmapp/urls.py
+++ wmapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('liked_songs', views.liked_songs, name='liked_songs'),
+    # path('playlists', views.playlists),
+
 ]
--- wmapp/views.py
+++ wmapp/views.py
@@ -1,5 +1,5 @@
 import random
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Playlist, OrdinaryUser, Account
 
 
@@ -26,3 +26,26 @@ def index(request):
         return render(request, 'index.html', context)
     else:
         return render(request, 'index.html')
+
+
+def liked_songs(request):
+    """View function for liked songs."""
+
+    if not request.user.is_authenticated:
+        return redirect('index')
+
+    # If admin without account, dont crash
+    if not Account.objects.filter(user=request.user).exists():
+        return redirect('index')
+
+    account = Account.objects.get(user=request.user)
+
+    if OrdinaryUser.objects.filter(account=account).exists():
+        ordinary_user = OrdinaryUser.objects.get(account=account)
+
+        context = {
+            'liked_songs': ordinary_user.liked_songs.order_by('title').all(),
+        }
+        return render(request, 'liked_songs.html', context)
+    else:
+        return redirect('index')
