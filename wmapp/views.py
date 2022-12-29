508e2a7f5d78a589187dd1db5d753ab162ef8179
Irene Bravo <irene.bravo.user@gmail.com>
2022-12-29 21:32:23 +0100

    decorators file added

--- wmapp/views.py
+++ wmapp/views.py
@@ -1,38 +1,8 @@
 import random
+from .decorators import require_ordinary_user, derive_user_type
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Playlist, OrdinaryUser, Account
-
-
-def require_ordinary_user(fun):
-    def wrapper(request):
-        if request.user_type == 'ordinary':
-            return fun(request)
-        return redirect('index')
-    return wrapper
-
-
-def derive_user_type(fun):
-    def wrapper(request):
-
-        if not request.user.is_authenticated:
-            request.user_type = None
-            return fun(request)
-
-        if not Account.objects.filter(user=request.user).exists():
-            request.user_type = 'admin'
-            return fun(request)
-
-        request.account = Account.objects.get(user=request.user)
-
-        if OrdinaryUser.objects.filter(account=request.account).exists():
-
-            request.user_type = 'ordinary'
-        else:
-            request.user_type = 'artist'
-
-        return fun(request)
-    return wrapper
 
 
 @derive_user_type
